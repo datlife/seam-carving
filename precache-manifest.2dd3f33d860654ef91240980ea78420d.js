4830c40f25e57e8b0965ed80e2105cfcff81b170
Dat Nguyen <tdat.nguyen93@gmail.com>
2018-10-31 23:06:11 -0700

    Updates

---
+++ precache-manifest.2dd3f33d860654ef91240980ea78420d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f17563b0f7d27880192",
+    "url": "/seam-carving/static/css/main.eaa64956.chunk.css"
+  },
+  {
+    "revision": "8f17563b0f7d27880192",
+    "url": "/seam-carving/static/js/main.8f17563b.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/seam-carving/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "55e2a35a13fc879b6da3",
+    "url": "/seam-carving/static/js/runtime~main.55e2a35a.js"
+  },
+  {
+    "revision": "320487615ec2a09511b9a06fbd99582d",
+    "url": "/seam-carving/index.html"
+  }
+];
