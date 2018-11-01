751c7b72237fa567ccabd54c7b15265b4fe47ea9
Dat Nguyen <tdat.nguyen93@gmail.com>
2018-10-31 22:28:18 -0700

    Changed to TypeScript

---
+++ seam_carving/src/App.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+            <h3>Seam Carving</h3>
+            <a href="" className="button-upload">Upload Your Photo</a>
+
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
